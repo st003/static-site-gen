befbafa860fe2c463628d43a5901aa7b08ccc061
Stuart Calcote <stuart.j.calcote@gmail.com>
2022-11-27 17:33:23 -0700

    Continued work on html minifier

--- sitegen/minifiers/html_minifier.py
+++ sitegen/minifiers/html_minifier.py
@@ -1,48 +1,84 @@
 """
 HTML minifier removes:
 
-1. Whitespace (tabs and spaces) not wrapped by html tags
-2. Newlines and carriage returns
+1. Whitespace not wrapped by html tags
+2. Tabs, newlines, and carriage returns
 3. Comments
 """
 
 import re
 from typing import Generator, Pattern
 
-# regex
+# regex patterns
+
+html_open: Pattern = re.compile(r'<')
+html_close_start: Pattern = re.compile(r'</')
+html_close_end: Pattern = re.compile(r'>')
+html_whitespace: Pattern = re.compile(r'\t|\n|\r|\f|\v')
+non_html_whitespace: Pattern = re.compile(r'\s')
 html_comment_start: Pattern = re.compile(r'<!--')
 html_comment_end: Pattern = re.compile(r'-->')
-whitespace: Pattern = re.compile(r'\s')
 
 # flags
+html_flag: bool = False
+html_close_flag: bool = False
 comment_flag: bool = False
 
 
 def minify_html(file_text: str) -> Generator[str, None, None]:
 
     global comment_flag
+    global html_flag
+    global html_close_flag
 
     for pos, char in enumerate(file_text):
 
-        # TODO - HTML tag wrappers
-        # TODO - inline css
-        # TODO - inline javascript
+        # html
+        if html_flag:
 
-        if (comment_flag):
+            if html_close_flag and html_close_end.match(char):
+                html_flag = False
+                html_close_flag = False
+
+            # look at the next char
+            elif html_close_start.match(file_text[pos:(pos + 2)]):
+                html_close_flag = True
+
+            else:
+                if html_whitespace.match(char):
+                    continue
+
+        else:
+
+            if html_open.match(char):
+                html_flag = True
+            else:
+                if non_html_whitespace.match(char):
+                    continue
+
+        # comments
+        if comment_flag:
+
             # look at last 3 chars
-            if html_comment_end.match(file_text[(pos - 2):(pos + 1)]):
+            comment_end_check: str = file_text[(pos - 2):(pos + 1)]
+
+            if html_comment_end.match(comment_end_check):
                 comment_flag = False
                 continue
             else:
                 continue
 
-        # looks at next 4 chars
-        elif html_comment_start.match(file_text[pos:(pos + 4)]):
-            comment_flag = True
-            continue
+        else:
 
-        elif whitespace.match(char):
-            continue
+            # look at next 4 chars for comment start
+            comment_start_check: str = file_text[pos:(pos + 4)]
+
+            if html_comment_start.match(comment_start_check):
+                comment_flag = True
+                continue
+
+        # TODO - inline css
+        # TODO - inline javascript
 
         yield char
 
--- tests/test_minify_html.py
+++ tests/test_minify_html.py
@@ -25,7 +25,7 @@ class TestHTMLMinifier(unittest.TestCase):
     def test_comment_with_text(self):
         """String with mix of comments and non-comments."""
 
-        comment = 'pre <!-- comment --> post'
+        comment = 'pre<!-- comment -->post'
         minifed_text = []
 
         for char in minify_html(comment):
@@ -37,26 +37,26 @@ class TestHTMLMinifier(unittest.TestCase):
     def test_space(self):
         """String with spaces only."""
 
-        comment = '     '
+        spaces = '     '
         minifed_text = []
 
-        for char in minify_html(comment):
+        for char in minify_html(spaces):
             minifed_text.append(char)
 
         minified_str = ''.join(minifed_text)
         self.assertEqual(len(minified_str), 0)
 
     def test_space_with_text(self):
-        """String with spaces only."""
+        """String with non-html wrapped spaces."""
 
-        comment = 'a b c'
+        comment = '   <h1>Hello, World!</h1>   '
         minifed_text = []
 
         for char in minify_html(comment):
             minifed_text.append(char)
 
         minified_str = ''.join(minifed_text)
-        self.assertEqual(minified_str, 'abc')
+        self.assertEqual(minified_str, '<h1>Hello, World!</h1>')
 
     def test_tabs(self):
         """String with tabs."""
@@ -93,3 +93,21 @@ class TestHTMLMinifier(unittest.TestCase):
 
         minified_str = ''.join(minifed_text)
         self.assertEqual(minified_str, 'abc')
+
+    def test_complex_html(self):
+        """A complex html file."""
+
+        with open('tests/html_minify_source.html', 'r', newline='') as html_src_file:
+            html = html_src_file.read()
+
+        minifed_text = []
+
+        for char in minify_html(html):
+            minifed_text.append(char)
+
+        minified_str = ''.join(minifed_text)
+
+        with open('tests/html_minify_expected.html', 'r', newline='') as html_expected_file:
+            expected_output = html_expected_file.read()
+
+        self.assertEqual(minified_str, expected_output)
