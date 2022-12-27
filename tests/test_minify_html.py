6e8e127efdcb59f13450f1565bcc2657299e51cc
Stuart Calcote <stuart.j.calcote@gmail.com>
2022-12-27 11:31:33 -0800

    Added type hints to html minifier tests

--- tests/test_minify_html.py
+++ tests/test_minify_html.py
@@ -10,139 +10,139 @@ from sitegen.minifiers.html_minifier import minify_html
 
 class TestHTMLMinifier(unittest.TestCase):
 
-    def test_tabs(self):
+    def test_tabs(self) -> None:
         """String with tabs."""
 
-        tabs = '<h1>pre\tpost</h1>\t'
-        minifed_text = []
+        tabs: str = '<h1>pre\tpost</h1>\t'
+        minifed_text: list[str] = []
 
         for char in minify_html(tabs):
             minifed_text.append(char)
 
-        minified_str = ''.join(minifed_text)
+        minified_str: str = ''.join(minifed_text)
         self.assertEqual(minified_str, '<h1>prepost</h1>')
 
-    def test_newlines(self):
+    def test_newlines(self) -> None:
         """String with newlines."""
 
-        newlines = '<h1>pre\npost</h1>\n'
-        minifed_text = []
+        newlines: str = '<h1>pre\npost</h1>\n'
+        minifed_text: list[str] = []
 
         for char in minify_html(newlines):
             minifed_text.append(char)
 
-        minified_str = ''.join(minifed_text)
+        minified_str: str = ''.join(minifed_text)
         self.assertEqual(minified_str, '<h1>prepost</h1>')
 
-    def test_carriage_returns(self):
+    def test_carriage_returns(self) -> None:
         """String with carriage returns."""
 
-        carriage_return = '<h1>abc</h1>\r'
-        minifed_text = []
+        carriage_return: str = '<h1>abc</h1>\r'
+        minifed_text: list[str] = []
 
         for char in minify_html(carriage_return):
             minifed_text.append(char)
 
-        minified_str = ''.join(minifed_text)
+        minified_str: str = ''.join(minifed_text)
         self.assertEqual(minified_str, '<h1>abc</h1>')
 
-    def test_neighboring_spaces(self):
+    def test_neighboring_spaces(self) -> None:
         """String with neighboring spaces."""
 
-        neighboring_spaces = '<h1>Hello,     World!</h1>'
-        minifed_text = []
+        neighboring_spaces: str = '<h1>Hello,     World!</h1>'
+        minifed_text: list[str] = []
 
         for char in minify_html(neighboring_spaces):
             minifed_text.append(char)
 
-        minified_str = ''.join(minifed_text)
+        minified_str: str = ''.join(minifed_text)
         self.assertEqual(minified_str, '<h1>Hello, World!</h1>')
 
-    def test_leading_whitespace(self):
+    def test_leading_whitespace(self) -> None:
         """String with leading whitespace."""
 
-        leading_whitespace = ' \t\n\r\f\v<!DOCTYPE html>'
-        minifed_text = []
+        leading_whitespace: str = ' \t\n\r\f\v<!DOCTYPE html>'
+        minifed_text: list[str] = []
 
         for char in minify_html(leading_whitespace):
             minifed_text.append(char)
 
-        minified_str = ''.join(minifed_text)
+        minified_str: str = ''.join(minifed_text)
         self.assertEqual(minified_str, '<!DOCTYPE html>')
 
-    def test_spaces_between_tags(self):
+    def test_spaces_between_tags(self) -> None:
         """Spaces between tags."""
 
-        spaces = '<div>   <p></p></div>'
-        minifed_text = []
+        spaces: str = '<div>   <p></p></div>'
+        minifed_text: list[str] = []
 
         for char in minify_html(spaces):
             minifed_text.append(char)
 
-        minified_str = ''.join(minifed_text)
+        minified_str: str = ''.join(minifed_text)
         self.assertEqual(minified_str, '<div><p></p></div>')
 
-    def test_html_tags_with_valid_spaces(self):
+    def test_html_tags_with_valid_spaces(self) -> None:
         """Inline spaces nested within html tags."""
 
-        inline_spaces = '<div> <p>Here is a <a>link</a> to click</p></div>'
-        minifed_text = []
+        inline_spaces: str = '<div> <p>Here is a <a>link</a> to click</p></div>'
+        minifed_text: list[str] = []
 
         for char in minify_html(inline_spaces):
             minifed_text.append(char)
 
-        minified_str = ''.join(minifed_text)
+        minified_str: str = ''.join(minifed_text)
         self.assertEqual(minified_str, '<div><p>Here is a <a>link</a> to click</p></div>')
 
-    def test_attributes(self):
+    def test_attributes(self) -> None:
         """Test html attributes."""
 
-        attributes = '<html lang="en"></html>'
-        minifed_text = []
+        attributes: str = '<html lang="en"></html>'
+        minifed_text: list[str] = []
 
         for char in minify_html(attributes):
             minifed_text.append(char)
 
-        minified_str = ''.join(minifed_text)
+        minified_str: str = ''.join(minifed_text)
         self.assertEqual(minified_str, '<html lang="en"></html>')
 
-    def test_comment(self):
+    def test_comment(self) -> None:
         """String with only comments."""
 
-        comment = '<!-- test comment -->'
-        minifed_text = []
+        comment: str = '<!-- test comment -->'
+        minifed_text: list[str] = []
 
         for char in minify_html(comment):
             minifed_text.append(char)
 
-        minified_str = ''.join(minifed_text)
+        minified_str: str = ''.join(minifed_text)
         self.assertEqual(len(minified_str), 0)
 
-    def test_inline_comment(self):
+    def test_inline_comment(self) -> None:
         """String with mix of comments and non-comments."""
 
-        comment = '<body>\n    <!-- comment -->\n    <h1>Hello, World!</h1>'
-        minifed_text = []
+        comment: str = '<body>\n    <!-- comment -->\n    <h1>Hello, World!</h1>'
+        minifed_text: list[str] = []
 
         for char in minify_html(comment):
             minifed_text.append(char)
 
-        minified_str = ''.join(minifed_text)
+        minified_str: str = ''.join(minifed_text)
         self.assertEqual(minified_str, '<body><h1>Hello, World!</h1>')
 
-    def test_html_only(self):
+    def test_html_only(self) -> None:
         """A complex html file."""
 
         with open('tests/html_minify_source.html') as html_file:
-            html = html_file.read()
+            html: str = html_file.read()
 
         with open('tests/html_minify_expected.html') as html_file:
-            expected = html_file.read()
+            expected: str = html_file.read()
 
-        minified_text = []
+        minified_text: list[str] = []
         for char in minify_html(html):
             minified_text.append(char)
 
-        minified_str = ''.join(minified_text)
+        minified_str: str = ''.join(minified_text)
 
         self.assertEqual(minified_str, expected)
