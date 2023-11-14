8d61469ba5f4c2ddc2a2d80cf7a31c8cecd3abd4
Stuart Calcote <stuart.j.calcote@gmail.com>
2023-11-14 15:30:43 -0700

    Added untracked constants.py

---
+++ sitegen/components/constants.py
@@ -0,0 +1,13 @@
+"""Constants used by components."""
+
+SNIPPET_PREFIX: str = '{{ '
+SNIPPET_SUFFIX: str = ' }}'
+
+TAG_BLOCK_END: str = '{% endblock %}'
+TAG_BLOCK_PREFIX: str = '{% block '
+
+TAG_LAYOUT_PREFIX: str = '{% layout '
+
+TAG_PATH_PREFIX: str = '{% path '
+
+TAG_SUFFIX: str = ' %}'
